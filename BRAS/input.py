4c1d6dd56f063061de44040783e155b0dedd2403
Hieu Le <166335731+HarrisonLe0211@users.noreply.github.com>
2024-08-02 03:35:27 +0000

    New reading script WIP

--- BRAS/input.py
+++ BRAS/input.py
@@ -1,4 +1,4 @@
 import pandas as pd
-df = pd.read_excel('GuestList.xlsx')
+df = pd.read_excel('GuestList.xlsx', engine="openpyxl",sheet_name="Sheet2")
 
-print(df)
+print(df.loc[2,'Name']) #Read cell at row 2 of column "Name"
